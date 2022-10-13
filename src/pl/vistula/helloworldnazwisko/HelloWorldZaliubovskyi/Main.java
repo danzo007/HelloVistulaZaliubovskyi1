6c845f59372340e495a35584e448149eca3d651c
Danyil Zaliuboskiy <dazno007@Danyils-MacBook-Pro.local>
2022-10-13 15:43:06 +0300

    Initial commit

---
+++ src/pl/vistula/helloworldnazwisko/HelloWorldZaliubovskyi/Main.java
@@ -0,0 +1,8 @@
+package pl.vistula.helloworldnazwisko.HelloWorldZaliubovskyi;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("Hello world1");
+    }
+}
